b34b13b7cf10aaf165c420a5ea61110ac2aebfdc
jongmin.lee <znargon@gmail.com>
2025-07-03 16:03:03 +0900

    1st - Check Read funtion

--- device_driver.cpp
+++ device_driver.cpp
@@ -1,12 +1,20 @@
 #include "device_driver.h"
+#include <stdexcept>
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int result = 0, temp = 0;
+    result = (int)(m_hardware->read(address));
+    for (int cnt = 0; cnt < 4;cnt++) {
+        temp = (int)(m_hardware->read(address));
+        if (temp != result) {
+            throw std::exception("Read Result is not same");
+        }
+    }
+    return result;
 }
 
 void DeviceDriver::write(long address, int data)
--- main.cpp
+++ main.cpp
@@ -1,12 +1,40 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
+#include <vector>
+#include <stdexcept>
 
-TEST(DeviceDriver, ReadFromHW) {
+using namespace std;
+using namespace testing;
+
+class MockFlashMemory : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+TEST(DeviceDriver, Read5times) {
 	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
-	DeviceDriver driver{ hardware };
-	int data = driver.read(0xFF);
-	EXPECT_EQ(0, data);
+	MockFlashMemory  mockHw;
+	DeviceDriver dd{ &mockHw };
+	EXPECT_CALL(mockHw, read(0xA))
+		.Times(5)
+		.WillRepeatedly(Return(0x77));
+
+	int result = dd.read(0xA);
+}
+
+TEST(DeviceDriver, ReadException) {
+	// TODO : replace hardware with a Test Double
+	MockFlashMemory  mockHw;
+	DeviceDriver dd{ &mockHw };
+
+	EXPECT_CALL(mockHw, read(0xA))
+		.WillOnce(Return(0x5))
+		.WillRepeatedly(Return(0x77));
+
+	EXPECT_THROW({
+		dd.read(0xA);
+		}, std::exception);
 }
 
 int main() {
